dbb88d38edfa3aed91f2319bddf05ec2f2101ca6
tihonov-ia <tia22@yandex.ru>
2024-07-27 11:10:41 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,64 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        title = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+        return title
+
+    def __eq__(self, other):  # 1
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):  # 2
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+
+lowtower = House('ЖК Эльбрус', 10)
+hightower = House('ЖК Акация', 20)
+
+print(hightower)
+print(lowtower)
+print(hightower == lowtower)
+print(lowtower.__add__(8))
+print(hightower == lowtower)
+print(hightower < lowtower)
+print(hightower <= lowtower)
+print(hightower > lowtower)
+print(hightower >= lowtower)
+print(hightower != lowtower)
+
